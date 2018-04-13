8049f572fdae2645ae703b954cc856d4317ba896
Željko Findak <zfindak@gmail.com>
2018-04-14 01:25:23 +0200

    Added build script

---
+++ GPSymbReg/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CC=/usr/local/cuda/bin/gcc
+export CXX=/usr/local/cuda/bin/g++
+
+rm -rf build/
+mkdir build
+cd build/
+
+cmake ..
+make
+
+# run
+#./GPSymbReg ../parameters.txt
